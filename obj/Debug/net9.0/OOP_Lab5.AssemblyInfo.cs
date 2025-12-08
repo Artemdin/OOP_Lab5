b090c9ca8bf662d1d1aa0a7d4d32a34f1dc250f7
Artemdin <dinovartem@gmail.com>
2025-12-08 19:39:11 +0200

    add gitignore

--- obj/Debug/net9.0/OOP_Lab5.AssemblyInfo.cs
+++ obj/Debug/net9.0/OOP_Lab5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP_Lab5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ccc229de62ccb1ba5ab1cb75f33795f2fe8b222")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP_Lab5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP_Lab5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
