f637d5b4a7569d2617b01977a8a9a3befe318743
Artemdin <dinovartem@gmail.com>
2025-12-08 20:12:29 +0200

    upload myfrac

--- MyFrac.cs
+++ MyFrac.cs
@@ -3,10 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace OOP_Lab5
 {
-    internal class MyFrac
+    internal class MyFrac : IMyNumber<MyFrac>
     {
+        private BigInteger nom;
+        private BigInteger denom;
+
+        public BigInteger Nom
+        {
+            get { return nom; }
+        }
+        public BigInteger Denom
+        {
+            get { return denom; }
+        }
+
+        public MyFrac(BigInteger nom, BigInteger denom)
+        {
+            this.nom = nom;
+            this.denom = denom;
+        }
+
+        public MyFrac Add(MyFrac b)
+        {
+            BigInteger newNom = nom * b.denom + b.nom * denom;
+            BigInteger newDen = denom * b.denom;
+            return new MyFrac(newNom, newDen);
+        }
+
+        public MyFrac Subtract(MyFrac b)
+        {
+            BigInteger newNom = nom * b.denom - b.nom * denom;
+            BigInteger newDen = denom * b.denom;
+            return new MyFrac(newNom, newDen);
+        }
+
+        public MyFrac Multiply(MyFrac b)
+        {
+            BigInteger newNom = nom * b.nom;
+            BigInteger newDen = denom * b.denom;
+            return new MyFrac(newNom, newDen);
+        }
+
+        public MyFrac Divide(MyFrac b)
+        {
+            if (b.nom == 0)
+                throw new DivideByZeroException();
+
+            BigInteger newNom = nom * b.denom;
+            BigInteger newDen = denom * b.nom;
+            return new MyFrac(newNom, newDen);
+        }
+
+        public override string ToString()
+        {
+            return $"{nom}/{denom}";
+        }
+
+
     }
 }
