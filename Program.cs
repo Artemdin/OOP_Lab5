5ccc229de62ccb1ba5ab1cb75f33795f2fe8b222
Artemdin <dinovartem@gmail.com>
2025-12-08 19:30:26 +0200

    creating classes

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using OOP_Lab5;
+using System;
+using System.Threading.Channels;
+
+namespace OOP_Lab5
+{
+
+    class Program
+    {
+        static void Main()
+        {
+          
+
+        }
+    }
+}
