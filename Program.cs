61ee9065fe1079e624de5604cbd1bba8789716e1
Artemdin <dinovartem@gmail.com>
2025-12-09 23:14:45 +0200

    correction menu

--- Program.cs
+++ Program.cs
@@ -1,119 +1,116 @@
-﻿using OOP_Lab5;
-using System;
+﻿using System;
 using System.Numerics;
-using System.Threading.Channels;
-using static System.Net.Mime.MediaTypeNames;
 
 namespace OOP_Lab5
 {
-
     class Program
     {
-            static void Main(string[] args)
-            {
-
+        static void Main(string[] args)
+        {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
+
             while (true)
-                {
+            {
                 Console.WriteLine("=== Виберіть дію ===");
-                    Console.WriteLine("1 - Робота з дробами (MyFrac)");
-                    Console.WriteLine("2 - Робота з комплексними числами (MyComplex)");
-                    Console.WriteLine("3 - Виконати unit-тести");
-                    Console.WriteLine("0 - Вийти з програми");
-                    Console.Write("Ваш вибір: ");
+                Console.WriteLine("1 - Тест формули (a+b)^2 на дробах");
+                Console.WriteLine("2 - Тест формули (a+b)^2 на комплексних");
+                Console.WriteLine("3 - Виконати unit-тести");
+                Console.WriteLine("0 - Вийти");
+                Console.Write("Ваш вибір: ");
 
-                    string input = Console.ReadLine();
-                    Console.WriteLine();
+                string input = Console.ReadLine();
+                Console.WriteLine();
 
-                    switch (input)
-                    {
-                        case "1":
-                            FracMenu();
-                            break;
-                        case "2":
-                            ComplexMenu();
-                            break;
-                        case "3":
-                            UnitTests.RunAllTests();
-                            break;
-                        case "0":
-                           Environment.Exit(0);
-                            break;
-                        default:
-                            Console.WriteLine("Невірний вибір, спробуйте ще раз.");
-                            break;
-                    }
-
-                    Console.WriteLine();
-                
-                }
-            }
-
-            static void FracMenu()
-            {
-                Console.WriteLine("=== Введіть два дроби ===");
-
-                Console.Write("Чисельник першого дробу: ");
-                BigInteger nom1 = BigInteger.Parse(Console.ReadLine());
-                Console.Write("Знаменник першого дробу: ");
-                BigInteger den1 = BigInteger.Parse(Console.ReadLine());
-
-                Console.Write("Чисельник другого дробу: ");
-                BigInteger nom2 = BigInteger.Parse(Console.ReadLine());
-                Console.Write("Знаменник другого дробу: ");
-                BigInteger den2 = BigInteger.Parse(Console.ReadLine());
-
-                MyFrac f1 = new MyFrac(nom1, den1);
-                MyFrac f2 = new MyFrac(nom2, den2);
-
-                Console.WriteLine("\nРезультати:");
-                Console.WriteLine("a + b = " + f1.Add(f2));
-                Console.WriteLine("a - b = " + f1.Subtract(f2));
-                Console.WriteLine("a * b = " + f1.Multiply(f2));
-
-                try
+                switch (input)
                 {
-                    Console.WriteLine("a / b = " + f1.Divide(f2));
+                    case "1":
+                        FracTest();
+                        break;
+
+                    case "2":
+                        ComplexTest();
+                        break;
+                   
+                    case "3":
+                        UnitTests.RunAllTests();
+                        break;
+
+                    case "0":
+                        return;
+
+                    default:
+                        Console.WriteLine("Невірний вибір!");
+                        break;
                 }
-                catch (DivideByZeroException)
-                {
-                    Console.WriteLine("Помилка: ділення на нуль!");
-                }
-            }
 
-            static void ComplexMenu()
-            {
-                Console.WriteLine("=== Введіть два комплексних числа ===");
-
-                Console.Write("Дійсна частина першого числа: ");
-                double re1 = double.Parse(Console.ReadLine());
-                Console.Write("Уявна частина першого числа: ");
-                double im1 = double.Parse(Console.ReadLine());
-
-                Console.Write("Дійсна частина другого числа: ");
-                double re2 = double.Parse(Console.ReadLine());
-                Console.Write("Уявна частина другого числа: ");
-                double im2 = double.Parse(Console.ReadLine());
-
-                MyComplex c1 = new MyComplex(re1, im1);
-                MyComplex c2 = new MyComplex(re2, im2);
-
-                Console.WriteLine($"\na = {c1}");
-               Console.WriteLine($"b = {c2}");
-            Console.WriteLine("\nРезультати:");
-                Console.WriteLine("a + b = " + c1.Add(c2));
-                Console.WriteLine("a - b = " + c1.Subtract(c2));
-                Console.WriteLine("a * b = " + c1.Multiply(c2));
-
-                try
-                {
-                    Console.WriteLine("a / b = " + c1.Divide(c2));
-                }
-                catch (DivideByZeroException)
-                {
-                    Console.WriteLine("Помилка: ділення на нуль!");
-                }
+                Console.WriteLine();
             }
         }
-    }
 
+        static void FracTest()
+        {
+            Console.WriteLine("=== Введіть два дроби ===");
+
+            Console.Write("Чисельник a: ");
+            BigInteger nomA = BigInteger.Parse(Console.ReadLine());
+            Console.Write("Знаменник a: ");
+            BigInteger denA = BigInteger.Parse(Console.ReadLine());
+
+            Console.Write("Чисельник b: ");
+            BigInteger nomB = BigInteger.Parse(Console.ReadLine());
+            Console.Write("Знаменник b: ");
+            BigInteger denB = BigInteger.Parse(Console.ReadLine());
+
+            MyFrac a = new MyFrac(nomA, denA);
+            MyFrac b = new MyFrac(nomB, denB);
+
+            FormulaTest(a, b);
+        }
+
+        static void ComplexTest()
+        {
+            Console.WriteLine("=== Введіть два комплексних числа ===");
+
+            Console.Write("Дійсна частина a: ");
+            double realA = double.Parse(Console.ReadLine());
+            Console.Write("Уявна частина a: ");
+            double imagiA = double.Parse(Console.ReadLine());
+
+            Console.Write("Дійсна частина b: ");
+            double reB = double.Parse(Console.ReadLine());
+            Console.Write("Уявна частина b: ");
+            double imB = double.Parse(Console.ReadLine());
+
+            MyComplex a = new MyComplex(realA, imagiA);
+            MyComplex b = new MyComplex(reB, imB);
+
+            FormulaTest(a, b);
+        }
+
+        static void FormulaTest<T>(T a, T b) where T : IMyNumber<T>
+        {
+            Console.WriteLine($"=== Starting testing (a+b)^2=a^2+2ab+b^2 with a = {a}, b = {b} ===");
+
+            T sum = a.Add(b);
+            T left = sum.Multiply(sum);
+
+            T a2 = a.Multiply(a);
+            T ab2 = a.Multiply(b).Add(a.Multiply(b)); 
+            T b2 = b.Multiply(b);
+
+            T right = a2.Add(ab2).Add(b2);
+
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"b = {b}");
+            Console.WriteLine($"(a + b) = {sum}");
+            Console.WriteLine($"(a+b)^2 = {left}");
+            Console.WriteLine("= = =");
+            Console.WriteLine($"a^2 = {a2}");
+            Console.WriteLine($"2*a*b = {ab2}");
+            Console.WriteLine($"b^2 = {b2}");
+            Console.WriteLine($"a^2+2ab+b^2 = {right}");
+
+            Console.WriteLine($"=== Finishing testing (a+b)^2=a^2+2ab+b^2 with a = {a}, b = {b} ===");
+        }
+    }
+}
